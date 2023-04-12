1f85deba3b733c308d17b763b7927678da3bf223
Gustavo Cassiano <gucpinto26@gmail.com>
2023-04-12 14:32:54 -0300

    Projeto finalizado

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,11 @@
+<template>
+    <header>
+        <h1>CALCULADORA ARITMÉTICA</h1>
+    </header>
+</template>
+
+<style scoped>
+    header {
+        padding: 30px 0px 10px;
+    }
+</style>
---
+++ src/components/Operacao.vue
@@ -0,0 +1,27 @@
+<script setup>
+    const props = defineProps(['operacao', 'num1', 'num2'])
+</script>
+
+<template>
+    <div class="operation">
+        <h2>Selecione a operação desejada:</h2>
+        <select @change="props.operacao">
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+        </select>
+    </div>
+    <div>
+        <input @keyup="props.num1" required type="number" placeholder="Entre com o 1° número">
+        <input @keyup="props.num2" required type="number" placeholder="Entre com o 2° número">
+    </div>
+</template>
+
+<style scoped>
+    input {
+        margin: 30px;
+        padding: 4px;
+        border-radius: 4px;
+    }
+</style>
